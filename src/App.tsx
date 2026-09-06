b719eacdeb9f90e9571c777f4548ec52d09a1114
Henri Schlupp <schlupphenri@gmail.com>
2026-09-06 21:57:00 +0300

    Set up react frontend

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>Music Video Prototype</h1>
+
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
+}
+
+export default App
