4febea583c7a586c8598825120404fc6d875780a
Henri Schlupp <schlupphenri@gmail.com>
2026-09-06 23:27:18 +0300

    Disabled mouse interaction on youtube player

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,52 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import YouTube, { type YouTubeEvent, type YouTubePlayer } from "react-youtube";
+import "./App.css";
 
 function App() {
   const playerRef = useRef<YouTubePlayer | null>(null);
+  const [showVideo, setShowVideo] = useState(false);
 
   const onReady = (event: YouTubeEvent) => {
     playerRef.current = event.target;
   };
 
-  const playVideo = () => {
-    playerRef.current?.playVideo();
-  };
+  const playClip = () => {
+    setShowVideo(false);
 
-  const pauseVideo = () => {
-    playerRef.current?.pauseVideo();
-  };
-
-  const playFiveSeconds = () => {
     playerRef.current?.playVideo();
 
+  
     setTimeout(() => {
+      setShowVideo(true);
+    }, 4350);
+
+  
+    setTimeout(() => {
+      setShowVideo(false);
       playerRef.current?.pauseVideo();
-    }, 5000);
+    }, 9000);
   };
 
   return (
     <div>
       <h1>Music Video Prototype</h1>
 
-      <YouTube
-        videoId="dQw4w9WgXcQ"
-        onReady={onReady}
-        opts={{
-          width: "640",
-          height: "360",
-          playerVars: {
-            controls: 0,
-            disablekb: 1,
-          },
-        }}
-      />
-
-      <div>
-        <button onClick={playVideo}>Play</button>
-        <button onClick={pauseVideo}>Pause</button>
-        <button onClick={playFiveSeconds}>Play 5 seconds</button>
+      <div className={`video-container ${showVideo ? "visible" : "hidden"}`}>
+        <YouTube
+          videoId="dQw4w9WgXcQ"
+          onReady={onReady}
+          opts={{
+            width: "640",
+            height: "360",
+            playerVars: {
+              controls: 0,
+              disablekb: 1,
+            },
+          }}
+        />
       </div>
+
+      <button onClick={playClip}>Play clip</button>
     </div>
   );
 }
