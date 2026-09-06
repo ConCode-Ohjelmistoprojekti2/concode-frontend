ee8dd4d991579c7d68532b35018a3efef14ab91b
Henri Schlupp <schlupphenri@gmail.com>
2026-09-06 22:17:58 +0300

    first working video WITH PROBLEMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
+import { useRef } from "react";
+import YouTube, { type YouTubeEvent, type YouTubePlayer } from "react-youtube";
+
 function App() {
+  const playerRef = useRef<YouTubePlayer | null>(null);
+
+  const onReady = (event: YouTubeEvent) => {
+    playerRef.current = event.target;
+  };
+
+  const playVideo = () => {
+    playerRef.current?.playVideo();
+  };
+
+  const pauseVideo = () => {
+    playerRef.current?.pauseVideo();
+  };
+
+  const playFiveSeconds = () => {
+    playerRef.current?.playVideo();
+
+    setTimeout(() => {
+      playerRef.current?.pauseVideo();
+    }, 5000);
+  };
+
   return (
     <div>
       <h1>Music Video Prototype</h1>
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <YouTube
+        videoId="dQw4w9WgXcQ"
+        onReady={onReady}
+        opts={{
+          width: "640",
+          height: "360",
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+          },
+        }}
+      />
+
+      <div>
+        <button onClick={playVideo}>Play</button>
+        <button onClick={pauseVideo}>Pause</button>
+        <button onClick={playFiveSeconds}>Play 5 seconds</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
