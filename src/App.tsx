16a354e6e527200a08588a7906109f94d2f041a5
Henri Schlupp <schlupphenri@gmail.com>
2026-09-14 03:46:11 +0300

    Connect frontend to random song API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import YouTube, { type YouTubeEvent, type YouTubePlayer } from "react-youtube";
 import "./App.css";
 
 function App() {
   const playerRef = useRef<YouTubePlayer | null>(null);
   const [showVideo, setShowVideo] = useState(false);
+  const [videoId, setVideoId] = useState("");
 
   const onReady = (event: YouTubeEvent) => {
     playerRef.current = event.target;
   };
+
+  const getRandomSong = async () => {
+    const response = await fetch("http://localhost:8080/api/songs/random");
+    const data = await response.json();
+    setVideoId(data.youtubeVideoId);
+  };
+
+  useEffect(() => {
+    getRandomSong();
+  }, []);
 
   const playClip = () => {
     setShowVideo(false);
 
     playerRef.current?.playVideo();
 
-  
     setTimeout(() => {
       setShowVideo(true);
     }, 4350);
 
-  
     setTimeout(() => {
       setShowVideo(false);
       playerRef.current?.pauseVideo();
@@ -32,21 +41,24 @@ function App() {
       <h1>Music Video Prototype</h1>
 
       <div className={`video-container ${showVideo ? "visible" : "hidden"}`}>
-        <YouTube
-          videoId="dQw4w9WgXcQ"
-          onReady={onReady}
-          opts={{
-            width: "640",
-            height: "360",
-            playerVars: {
-              controls: 0,
-              disablekb: 1,
-            },
-          }}
-        />
+        {videoId && (
+          <YouTube
+            videoId={videoId}
+            onReady={onReady}
+            opts={{
+              width: "640",
+              height: "360",
+              playerVars: {
+                controls: 0,
+                disablekb: 1,
+              },
+            }}
+          />
+        )}
       </div>
 
       <button onClick={playClip}>Play clip</button>
+      <button onClick={getRandomSong}>New random song</button>
     </div>
   );
 }
